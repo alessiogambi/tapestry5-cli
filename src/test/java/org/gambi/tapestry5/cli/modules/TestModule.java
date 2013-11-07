30517fdd22100b0423eea6c1d367166ea2d03790
Alessio Gambi <alessio.gambi@usi.ch>
2013-11-07 15:45:05 +0100

    Basic tests to check parsing

--- src/test/java/org/gambi/tapestry5/cli/modules/TestModule.java
+++ src/test/java/org/gambi/tapestry5/cli/modules/TestModule.java
@@ -1,33 +1,25 @@
 package org.gambi.tapestry5.cli.modules;
 
-import org.apache.commons.cli.Option;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.gambi.tapestry5.cli.data.BeanA;
 import org.gambi.tapestry5.cli.data.BeanFOO;
+import org.gambi.tapestry5.cli.data.CLIOption;
 
 public class TestModule {
 
-	// @Inject
-	// private volatile Messages messages;
-
-	// public void contributeCLIParser(Configuration<Option> configuration) {
-	//
-	// configuration.add(new Option("a", "alfa-option", true, messages
-	// .get("alfa-description")));
-	//
-	// configuration.add(new Option("b", "beta", true, messages
-	// .get("beta-description")));
-	//
-	// configuration.add(new Option("g", "this-is-gamma", true, messages
-	// .get("This is gamma")));
-	//
-	// }
-
-	public void contributeCLIParser(Configuration<Option> configuration) {
-		configuration.add(new Option("a", "alfa", true, "alfa-description"));
-		configuration.add(new Option("b", "beta", true, "beta-description"));
-		configuration.add(new Option("g", "gamma", true, "This is gamma"));
+	/**
+	 * Define some sample contributions
+	 * 
+	 * @param configuration
+	 */
+	public void contributeCLIParser(Configuration<CLIOption> configuration) {
+		configuration.add(new CLIOption("a", "alfa", 1, true,
+				"alfa-description"));
+		configuration.add(new CLIOption("b", "beta", 0, true,
+				"beta-description"));
+		configuration
+				.add(new CLIOption("g", "gamma", 1, false, "This is gamma"));
 	}
 
 	public void contributeApplicationConfigurationSource(
